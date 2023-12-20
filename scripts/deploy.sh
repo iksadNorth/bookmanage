dc647017606a71332dbbbdf4d3fccd5392ebef06
iksadNorth <iksadNorth@gmail.com>
2023-12-21 00:51:56 +0900

    appspec.yml 추가

---
+++ scripts/deploy.sh
@@ -0,0 +1,1 @@
+java -jar build/libs/rmsoft-0.0.1-SNAPSHOT.jar
---
+++ scripts/install_codedeploy_agent.sh
@@ -0,0 +1,7 @@
+sudo apt-get update
+sudo apt-get install ruby
+sudo apt-get install wget
+cd /home/ubuntu
+wget https://aws-codedeploy-ap-northeast-2.s3.ap-northeast-2.amazonaws.com/latest/install
+chmod +x ./install
+sudo ./install auto
