3221295a29f03ad75204dbcd2c83491badaa9f04
iksadNorth <iksadNorth@gmail.com>
2023-12-21 14:38:05 +0900

    스크립트의 파일 위치 언급 시, 절대경로를 이용

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,5 +1,7 @@
-sudo chmod +x build/libs/rmsoft-0.0.1-SNAPSHOT.jar
-nohup java -jar build/libs/rmsoft-0.0.1-SNAPSHOT.jar &
+ROOT_DIR=/home/ubuntu;
+
+sudo chmod +x $ROOT_DIR/build/libs/rmsoft-0.0.1-SNAPSHOT.jar
+nohup java -jar $ROOT_DIR/build/libs/rmsoft-0.0.1-SNAPSHOT.jar &
 
 echo "$(date) | run deploy.sh"
 echo "$(pwd) | run deploy.sh"
